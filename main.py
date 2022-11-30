ac93057d98bc45dd8ec87d248791a918489c7890
Brian Toone <115804135+kartoone76@users.noreply.github.com>
2022-11-30 17:25:20 +0000

    added unit tests

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 '''Defines some geometric functions'''
-import unittest
 
-def square(a_n):
-    '''Takes in a number n, returns the square of n'''
-    return a_n*a_n
 
-class Test(unittest.TestCase):
-    def test_square(self):
-        self.assertEqual(square(2), 4)
-  
+def square(n):
+  '''Takes in a number n, returns the square of n'''
+  return n * n
+
+
+def cube(n):
+  '''Takes in a number n, returns the cube of n'''
+  return n * n * n
